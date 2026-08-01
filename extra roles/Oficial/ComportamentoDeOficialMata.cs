857dba36c56c7b1cbd73056df853728df0255b28
217modG <at8332904@gmail.com>
2026-08-01 06:52:31 -0300

    Create ComportamentoDeOficialMata.cs

---
+++ extra roles/Oficial/ComportamentoDeOficialMata.cs
@@ -0,0 +1,14 @@
+usando  o sistema ;
+usando  System.Collections.Generic ;
+usando  System.Text ;
+
+namespace  ExtraRoles . Oficial
+{
+     enum  público OfficerKillBehaviour
+    {
+        Impostor  =  0 ,
+        Coringa  =  1 ,
+        CrewDie  =  2 ,
+        O policial sobrevive  =  3
+    }
+}
